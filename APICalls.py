fdc6c241617f6c2c8ac8f393be0393bdd578a9db
SlashDK <slashdk@github.com>
2016-10-08 19:58:01 -0400

    Get companyname, heroku remain

--- APICalls.py
+++ APICalls.py
@@ -28,8 +28,14 @@ endDate=%s&identifiers=%s&outputDataExpression=resultMap%%5B\
 'RETURNS'%%5D%%5B0%%5D.latestPerf%%5B'sinceStartDate'%%5D&startDate=%s&useCache=true"""%(endDate,ticker,startDate) 
 	r1=requests.get(callOneDay)
 	r2=requests.get(callFromStart)
+	print(type(r1.content),r1.content)
+	r1=str(r1.content)
+	r1=r1[2:r1.find('.')+4]
+	r2=str(r2.content)
+	r2=r2[2:r2.find('.')+4]
 	# r = requests.post("http://yoururl/post", data={'foo': 'bar'})
-	return str((r1.content,r2.content))
+	obj = {u"oneDayChange": r1, u"sinceStartDateChange": r2}
+	return json.dumps(obj)
 
 print(APICalls('apple'))
 
